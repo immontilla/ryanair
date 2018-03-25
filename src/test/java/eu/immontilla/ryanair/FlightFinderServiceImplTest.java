e7dbe4cf32963363e49144decbdeebd1cce67dcd
immontilla <mauricio@immontilla.eu>
2018-03-25 19:59:03 +0200

    Testing update

--- src/test/java/eu/immontilla/ryanair/FlightFinderServiceImplTest.java
+++ src/test/java/eu/immontilla/ryanair/FlightFinderServiceImplTest.java
@@ -1,5 +1,6 @@
 package eu.immontilla.ryanair;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
@@ -63,9 +64,10 @@ public class FlightFinderServiceImplTest {
     public void setUp() {
         List<Route> routes = new ArrayList<Route>();
         routes.add(new Route(CRL, DUB, null, false, false, RYANAIR, ANYTHING));
-        routes.add(new Route(CRL, LIS, null, false, false, RYANAIR, ANYTHING));
-        routes.add(new Route(DUB, MAD, null, false, false, RYANAIR, ANYTHING));
+        routes.add(new Route(MAD, DUB, null, false, false, RYANAIR, ANYTHING));
+        routes.add(new Route(DUB, LIS, null, false, false, RYANAIR, ANYTHING));
         routes.add(new Route(LGW, DUB, null, false, false, RYANAIR, ANYTHING));
+        routes.add(new Route(DUB, MAD, null, false, false, RYANAIR, ANYTHING));
         Mockito.when(availableRouteService.getAll()).thenReturn(routes);
     }
 
@@ -98,8 +100,105 @@ public class FlightFinderServiceImplTest {
         Schedule schedule = new Schedule(month, days);
         Mockito.when(scheduleFinderService.get(DUB, MAD, month, year)).thenReturn(schedule);
         List<FlightResult> flightResults = flightFinderService.findFlights(DUB, MAD, tomorrow, afterTomorrow);
-        assertTrue(!flightResults.isEmpty());
-        assertTrue(flightResults.size() == 1);
+        assertEquals(0, flightResults.get(0).getStops());
+    }
+
+    @Test
+    public void whenLookForFlightInRouteWithScheduleNextYearThenReturnScheduleList() {
+        int day = 28;
+        int month = 12;
+        int year = now.getYear();
+        LocalDateTime december28th = LocalDateTime.of(year, month, day, 6, 0);
+        String departureDateTime = december28th.plusMinutes(30)
+                .format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        String arrivalDateTime = december28th.plusHours(4).format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        List<DayFlight> days = new ArrayList<DayFlight>();
+        List<Flight> flights = new ArrayList<Flight>();
+        flights.add(new Flight("7177", departureDateTime, arrivalDateTime));
+        days.add(new DayFlight(day, flights));
+        Schedule schedule = new Schedule(month, days);
+        Mockito.when(scheduleFinderService.get(DUB, MAD, 12, year)).thenReturn(schedule);
+        month = 1;
+        days = new ArrayList<DayFlight>();
+        flights = new ArrayList<Flight>();
+        departureDateTime = december28th.plusDays(4).plusMinutes(30)
+                .format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        arrivalDateTime = december28th.plusDays(4).plusHours(4)
+                .format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        flights.add(new Flight("7277", departureDateTime, arrivalDateTime));
+        days.add(new DayFlight(day, flights));
+        Schedule scheduleNextYearJanuary = new Schedule(month, days);
+        Mockito.when(scheduleFinderService.get(DUB, MAD, month, year + 1)).thenReturn(scheduleNextYearJanuary);
+        List<FlightResult> flightResults = flightFinderService.findFlights(DUB, MAD, december28th,
+                december28th.plusDays(5));
+        assertEquals(0, flightResults.get(0).getStops());
+    }
+
+    @Test
+    public void whenLookForFlightNoDirectOneStopAvailableNoScheduleThenReturnEmptyList() {
+        int month = tomorrow.getMonthValue();
+        int year = tomorrow.getYear();
+        Mockito.when(scheduleFinderService.get(MAD, DUB, month, year)).thenReturn(null);
+        List<FlightResult> flightResults = flightFinderService.findFlights(MAD, LIS, tomorrow, afterTomorrow);
+        assertTrue(flightResults.isEmpty());
+    }
+
+    @Test
+    public void whenLookForFlightNoDirectOneStopAvailableWithScheduleThenReturnFlightResultList() {
+        int day = tomorrow.getDayOfMonth();
+        int month = tomorrow.getMonthValue();
+        int year = tomorrow.getYear();
+        String departureDateTime = tomorrow.plusMinutes(30).format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        String arrivalDateTime = tomorrow.plusHours(4).format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        List<DayFlight> days = new ArrayList<DayFlight>();
+        List<Flight> flights = new ArrayList<Flight>();
+        flights.add(new Flight("7177", departureDateTime, arrivalDateTime));
+        days.add(new DayFlight(day, flights));
+        Schedule scheduleMADDUB = new Schedule(month, days);
+        Mockito.when(scheduleFinderService.get(MAD, DUB, month, year)).thenReturn(scheduleMADDUB);
+        days = new ArrayList<DayFlight>();
+        flights = new ArrayList<Flight>();
+        LocalDateTime connection = tomorrow.plusHours(7);
+        day = connection.getDayOfMonth();
+        month = connection.getMonthValue();
+        year = connection.getYear();
+        departureDateTime = connection.format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        arrivalDateTime = connection.plusHours(3).format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        flights.add(new Flight("7889", departureDateTime, arrivalDateTime));
+        days.add(new DayFlight(day, flights));
+        Schedule scheduleDUBLIS = new Schedule(month, days);
+        Mockito.when(scheduleFinderService.get(DUB, LIS, month, year)).thenReturn(scheduleDUBLIS);
+        List<FlightResult> flightResults = flightFinderService.findFlights(MAD, LIS, tomorrow, afterTomorrow);
+        assertEquals(1, flightResults.get(0).getStops());
+    }
+
+    @Test
+    public void whenLookForFlightNoDirectOneStopNoScheduleAvailableThenReturnFlightResultList() {
+        int day = tomorrow.getDayOfMonth();
+        int month = tomorrow.getMonthValue();
+        int year = tomorrow.getYear();
+        String departureDateTime = tomorrow.plusMinutes(30).format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        String arrivalDateTime = tomorrow.plusHours(4).format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        List<DayFlight> days = new ArrayList<DayFlight>();
+        List<Flight> flights = new ArrayList<Flight>();
+        flights.add(new Flight("7177", departureDateTime, arrivalDateTime));
+        days.add(new DayFlight(day, flights));
+        Schedule scheduleMADDUB = new Schedule(month, days);
+        Mockito.when(scheduleFinderService.get(MAD, DUB, month, year)).thenReturn(scheduleMADDUB);
+        days = new ArrayList<DayFlight>();
+        flights = new ArrayList<Flight>();
+        LocalDateTime connection = tomorrow.plusHours(5);
+        day = connection.getDayOfMonth();
+        month = connection.getMonthValue();
+        year = connection.getYear();
+        departureDateTime = connection.format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        arrivalDateTime = connection.plusHours(3).format(DateTimeFormatter.ofPattern(ONLY_HOUR_AND_MINUTE));
+        flights.add(new Flight("7889", departureDateTime, arrivalDateTime));
+        days.add(new DayFlight(day, flights));
+        Schedule scheduleDUBLIS = new Schedule(month, days);
+        Mockito.when(scheduleFinderService.get(DUB, LIS, month, year)).thenReturn(scheduleDUBLIS);
+        List<FlightResult> flightResults = flightFinderService.findFlights(MAD, LIS, tomorrow, afterTomorrow);
+        assertTrue(flightResults.isEmpty());
     }
 
 }
